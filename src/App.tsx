41e8858e675be6dbfa13d098fa23dbfa83b03f9b
igorlameirao <igorlameirao@hotmail.com>
2022-03-31 11:51:03 -0300

    Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from './Components/Button'
+
+function App() {
+  return (
+    <div>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
